4740add37d41cf550bbb2da17f21c54c47d7d031
FerGutierR2302 <fernando.gutierrezr@iteso.mx>
2024-11-08 13:53:16 -0600

    Estrategia de Benchmark (Pasiva)

---
+++ gans_strategy/backtesting.py
@@ -0,0 +1,4 @@
+def passive_strategy(prices, initial_balance=10000):
+    positions = initial_balance / prices['Close'][0]  # Comprar al inicio
+    final_balance = positions * prices['Close'].iloc[-1]  # Valor al final
+    return final_balance
