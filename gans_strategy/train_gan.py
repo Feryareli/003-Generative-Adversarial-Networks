b522ea30d985116d7ad6b4066c795d78522ee5cd
a1111 <diegoatg02@gmail.com>
2024-11-08 20:59:24 -0600

    primera correccion del train_gan

--- gans_strategy/train_gan.py
+++ gans_strategy/train_gan.py
@@ -1,14 +1,12 @@
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers, models
 
 # Construir Generador
-
 def build_generator(latent_dim=300, seq_len=252):
     model = models.Sequential([
         layers.Input(shape=(latent_dim,)),
         layers.Dense(256, activation='relu'),
-         layers.Dense(256, activation='relu'),
+        layers.Dense(256, activation='relu'),
         layers.Dense(seq_len, activation='linear')  # Salida con una característica por paso
     ])
     return model
@@ -23,21 +21,9 @@ def build_discriminator(seq_len=252):
     ])
     return model
 
-# Función para entrenar el GAN
-def train_gan(generator, discriminator, x_train_norm, epochs=1000, batch_size=100, clip_value=1):
-    gen_loss_history = []
-    disc_loss_history = []
-
-    # Optimizadores para el generador y el discriminador
-    generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-    discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-
-    # Número de lotes
-    num_batches = (len(x_train_norm) // batch_size) - 1
-
-
+# Funcion de paso
 @tf.function
-def train_step(real_data):
+def train_step(real_data, generator, discriminator, generator_optimizer, discriminator_optimizer, batch_size=100, clip_value=1):
     noise = tf.random.normal([batch_size, generator.input_shape[-1]])
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
@@ -65,21 +51,3 @@ def train_step(real_data):
         var.assign(tf.clip_by_value(var, -clip_value, clip_value))
 
     return gen_loss, disc_loss
-
-    # Entrenar por varias épocas
-    num_batches = (len(x_train_norm) // 252) -1
-    for epoch in range(epochs):
-        for batch in range(num_batches):
-            batch = x_train_norm[batch*252:(batch+1)*252]
-            gen_loss, disc_loss = train_step(batch, generator, discriminator)
-            gen_loss_history.append(gen_loss)
-            disc_loss_history.append(disc_loss)
-
-    # Cada ciertas iteration, imprime las pérdidas
-    print(f"Epoch {epoch}, Gen Loss: {gen_loss.numpy()}, Disc Loss: {disc_loss.numpy()}")
-
-
-
-
-
-
